ebffb3350cf9c877960e11524fee24ddcbd89e4c
Aaron Jones <aaronjjj95@aol.com>
2024-04-25 22:18:45 +0000

    ability to delete posts created

--- travel_blog/forms.py
+++ travel_blog/forms.py
@@ -13,3 +13,16 @@ class PostForm(forms.ModelForm):
             'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Insert Your Blog Post Here'}),
 
         }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'title_tag', 'body')
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Insert Title Here'}),
+            'title_tag': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Insert Title_tag Here'}),
+            #'author': forms.Select(attrs={'class': 'form-control'}),
+            'body': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Insert Your Blog Post Here'}),
+
+        }
