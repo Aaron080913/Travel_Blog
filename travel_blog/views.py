46ae1bf02a1b9eca14dfb5f164ba6aac4c71b47e
Aaron Jones <aaronjjj95@aol.com>
2024-04-29 21:57:47 +0000

    added like button functionality

--- travel_blog/views.py
+++ travel_blog/views.py
@@ -11,7 +11,14 @@ from django.http import HttpResponseRedirect
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('article-detail', args=[str(pk)]))
 
 
@@ -46,8 +53,13 @@ class ArticleDetailView(DetailView):
         context = super(ArticleDetailView, self).get_context_data(*args, **kwargs)
         stuff = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = stuff.total_likes()
+
+        liked = False
+        if stuff.likes.filter(id=self.kwargs['pk']):
+            liked = True
         context ["total_likes"] = total_likes
         context["cat_menu"] = cat_menu
+        context["liked"] = liked
         return context
 
 class AddPostView(CreateView):
