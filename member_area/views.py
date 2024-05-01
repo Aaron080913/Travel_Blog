3e1a0bf387fd01bf0578433039b66fed47f4f0e4
Aaron Jones <aaronjjj95@aol.com>
2024-05-01 21:18:56 +0000

    debugging

--- member_area/views.py
+++ member_area/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
 from .forms import SignUpForm, EditProfileForm
 
 # Create your views here.
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordChangeForm
+    template_name = 'registration/change-password.html'
+    success_url = reverse_lazy('home')
+
+
+
 class UserRegisterView(generic.CreateView):
     form_class = SignUpForm
     template_name = 'registration/register.html'
